f7ee1564782760480a81d8d98dbf78fd655705fa
unknown <lamichhaneparikshya75@gmail.com>
2024-07-19 21:11:54 +0545

    First commit

---
+++ base/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Todo(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=100)  
