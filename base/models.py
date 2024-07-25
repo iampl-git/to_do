5d78b9a75b43561aaebd7332feede620f9eb262d
unknown <lamichhaneparikshya75@gmail.com>
2024-07-25 12:38:51 +0545

    commit

--- base/models.py
+++ base/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    status = models.CharField(max_length=100)  
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Todo
 
-
-# Create your views here.
 def home(request):
-    return HttpResponse('This is a home page')
+    todos = Todo.objects.all()
+    return render(request, 'index.html', {'todos': todos})
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        Todo.objects.create(name=name, description=description, status=status)
+        return redirect('home')
+    return render(request, 'create.html')
+
+def edit(request):
+    print("GET ID:", request.GET.get('id'))  # Debugging line
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        todo = get_object_or_404(Todo, id=id)
+        
+        todo.name = request.POST.get('name')
+        todo.description = request.POST.get('description')
+        todo.status = request.POST.get('status')
+        todo.save()
+        return redirect('home')
+    
+    id = request.GET.get('id')
+    if not id:
+        return redirect('home')
+        
+    todo = get_object_or_404(Todo, id=id)
+    return render(request, 'edit.html', {'todo': todo})
 
